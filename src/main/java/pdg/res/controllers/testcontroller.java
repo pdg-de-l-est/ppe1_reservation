b6e68812cedb37d27ab048374186e4870fb699b5
Sam Potentier <spotentier@sio.local>
2020-10-15 09:21:17 +0200

    new product

--- src/main/java/pdg/res/controllers/testcontroller.java
+++ src/main/java/pdg/res/controllers/testcontroller.java
@@ -2,10 +2,12 @@ package pdg.res.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import io.github.jeemv.springboot.vuejs.VueJS;
+import io.github.jeemv.springboot.vuejs.utilities.Http;
 
 @Controller
 public class testcontroller {
@@ -20,6 +22,17 @@ public class testcontroller {
 	
 	@RequestMapping("/test")
 	public String index() {
+		return "newProduct";
+	}
+	
+	@RequestMapping("/test")
+	public String index(ModelMap model) {
+		vue.addData("dialog", false);
+		vue.addData("isNew", false);
+		vue.addDataRaw("Product", "{}");
+		vue.addDataRaw("Products", "[]");
+		vue.addMethod("addProduct", "let self=this;" + Http.post("Product", "self.Product", "self.Products.push(response.data);self.dialog=false;self.showMsg('success','Organisation '+self.orga.name+' ajoutée !');", "self.showMsg('error',response);"));
 		return "index";
 	}
 }
+
