8dd326eafb5a03845d0c9e27244c5d721c66068a
Sam Potentier <spotentier@sio.local>
2020-11-03 16:34:10 +0100

    recherche produit par type

--- src/main/java/pdg/res/repositories/ProductRepository.java
+++ src/main/java/pdg/res/repositories/ProductRepository.java
@@ -1,5 +1,6 @@
 package pdg.res.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import pdg.res.models.Product;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
 	public Optional<Product> findByTitle(String title);
+	public List<Product> findByType(String type);
 }
